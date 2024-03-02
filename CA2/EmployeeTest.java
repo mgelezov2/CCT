7aa8856f9de43d89aae6342c341ce13814a6501c
mgelezov2 <115002474+mgelezov2@users.noreply.github.com>
2024-03-02 11:15:01 +0000

    ADDS PROJECT GROUP ARRAY FOR STORING OBJECTS AND M ARRAY FOR STORING A TARGETED EMPLOYEE

--- CA2/EmployeeTest.java
+++ CA2/EmployeeTest.java
@@ -10,3 +10,13 @@ import java.util.List;
 
 //EMPLOYEE TEST THAT EXTENDS TO THE EMPLOYEE CLASS ALLOWING USAGE OF METHODS FROM THE EMPLOYEE CLASS
 public class EmployeeTest extends Employee{
+
+    public static void mainCall() {
+      
+//PROJECTGROUP ARRAY FOR STORING EMPLOYEE OBJECTS; NAME, EMAIL AND EMPLOYEE NUMBER
+        ArrayList<String> projectGroup = new ArrayList<String>();
+        ArrayList<Integer> empNums = new ArrayList<Integer>();
+      
+// M ARRAY FOR STORING A SPECIFIC EMPLOYEE THAT IS SELECTED BY AN EMPLOYEE NUMBER
+        List<String> m = new ArrayList<String>();
+      
