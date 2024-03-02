2a957957212c4fefb10b1175e04b1b4133e6fd22
mgelezov2 <115002474+mgelezov2@users.noreply.github.com>
2024-03-02 11:13:33 +0000

    ADDS JAVA CLASS EmployeeTest THAT EXTENDS TO Employee

--- CA2/EmployeeTest.java
+++ CA2/EmployeeTest.java
@@ -1,1 +1,12 @@
+package ca2_ooa;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author michael
+ */
+
+//EMPLOYEE TEST THAT EXTENDS TO THE EMPLOYEE CLASS ALLOWING USAGE OF METHODS FROM THE EMPLOYEE CLASS
+public class EmployeeTest extends Employee{
