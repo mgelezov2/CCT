0233f0b3a3a292ffce0f2f62a0dda7aa7a53d717
mgelezov2 <115002474+mgelezov2@users.noreply.github.com>
2024-03-02 11:22:20 +0000

    ADDS COMPANY CLASS WITH DEFAULT VALUES AND INITIALIZED CONSTRUCTOR

--- CA2/Company_base.java
+++ CA2/Company_base.java
@@ -15,3 +15,14 @@ public class Company_base extends Employee{
     Employee employee_comp = new Employee();
     String company_Name;
     ArrayList<String> staff = new ArrayList<>();
+
+//COMPANY CLASS CONSTRUCTOR
+     public Company_base(){
+        company_Name = "Business Gnómes Ltd";
+        
+    }
+     
+//INITALIZED COMPANY CONSTRUCTOR
+    public Company_base(String companyName){
+        company_Name = companyName;
+    }
