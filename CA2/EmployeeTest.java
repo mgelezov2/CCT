07f3bf9900b6a97f19a1c1a8e44224d2eff67221
mgelezov2 <115002474+mgelezov2@users.noreply.github.com>
2024-03-02 11:18:20 +0000

    ADDED FUNCTION TO ADD OBJECTS TO DESIGNATED ARRAYS
    EMAIL CHECK IS ADDED TO EMPLOYEE OBJECTS TO CHECK IF EMAIL FORMAT IS FOLLOWED

--- CA2/EmployeeTest.java
+++ CA2/EmployeeTest.java
@@ -43,3 +43,37 @@ public class EmployeeTest extends Employee{
                empObject3Email = empObj3.getEmail(empObj3.setEmail("tt@gmail.com"));
         
         int empObjectNum3 = empObj3.getEmpNum(542134);
+
+        //OBJECTS BELOW ARE STORED IN AN ARRAY AND AN EMAIL CHECK TO MAKE SURE IT FOLLOWS @GMAIL.COM FORMAT
+if(empObject1Email.contains("@gmail.com") && empObject2Email.contains("@gmail.com") && empObject3Email.contains("@gmail.com")){
+            projectGroup.add(empObject1Name);
+            projectGroup.add(empObject1Email);
+            empNums.add(empObjectNum1);
+        
+            projectGroup.add(empObject2Name);
+            projectGroup.add(empObject2Email);
+            empNums.add(empObjectNum2);
+
+            projectGroup.add(empObject3Name);
+            projectGroup.add(empObject3Email);
+            empNums.add(empObjectNum3);
+}else{
+    System.out.println("Error invalid email format");
+}
+        
+        
+        
+        //System.out.println(projectGroup + "\n");
+        //System.out.println(empNums + "\n");
+        //System.out.println(empObj1.getNextEmpNum(123456));
+        
+// IF EMPLOYEE EXISTS WITH THIS EMPLOYEE NUMBER IT IS STORED IN M ARRAY IF NOT ERROR IS OUTPUTTED
+        if(empNums.contains(123456)){
+            System.out.println(empObj1.getName(empObject1Name) + " " + empNums.get(0));
+            //m.add(empObj1.getName(empObject1Name));
+            //System.out.println(m);
+        }else{
+            System.out.println("Employee does not exist");
+        }
+        
+}
