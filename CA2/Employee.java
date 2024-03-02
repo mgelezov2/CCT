24f87b142af632ef2a02af274d83029f6e20f2d9
mgelezov2 <115002474+mgelezov2@users.noreply.github.com>
2024-03-02 11:05:40 +0000

    ADDED EMPLOYEE CLASS

--- CA2/Employee.java
+++ CA2/Employee.java
@@ -1,1 +1,16 @@
+package ca2_ooa;
 
+
+
+/**
+ *
+ * @author michael
+ */
+
+//EMPLOYEE CLASS
+
+public class Employee {
+       public String name;
+       public String email;
+       public int empNum;
+       public static int nextEmpNum = 1;
