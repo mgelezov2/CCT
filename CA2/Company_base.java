6349ac540b74949aa4c68c3f4853b6bca65f9e53
mgelezov2 <115002474+mgelezov2@users.noreply.github.com>
2024-03-02 11:21:43 +0000

    ADDS COMPANY CLASS EXTENDING TO EMPLOYEE CLASS

--- CA2/Company_base.java
+++ CA2/Company_base.java
@@ -1,1 +1,17 @@
+package ca2_ooa;
 
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ *
+ * @author michael
+ */
+
+//COMPANY CLASS THAT EXTENDS TO EMPLOYEE FOR USAGE OF EMPLOYEE METHODS
+public class Company_base extends Employee{
+    Employee employee_comp = new Employee();
+    String company_Name;
+    ArrayList<String> staff = new ArrayList<>();
