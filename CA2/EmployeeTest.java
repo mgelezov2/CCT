7f7735f79e487215390c530837b5377e8aeb6e9e
mgelezov2 <115002474+mgelezov2@users.noreply.github.com>
2024-03-02 11:16:08 +0000

    ADDED EMPLOYEE OBJECTS FOR TESTING METHODS

--- CA2/EmployeeTest.java
+++ CA2/EmployeeTest.java
@@ -20,3 +20,26 @@ public class EmployeeTest extends Employee{
 // M ARRAY FOR STORING A SPECIFIC EMPLOYEE THAT IS SELECTED BY AN EMPLOYEE NUMBER
         List<String> m = new ArrayList<String>();
       
+//EMPLOYEE OBJECTS THAT ARE STORED INTO AN ARRAY
+        Employee empObj1 = new Employee();
+        Employee empObj2 = new Employee();
+        Employee empObj3 = new Employee();
+        
+//EMPLOYEE OBJECTS THAT ARE INITALIZED
+        String empObject1Name = empObj1.getName("Joe Bloggs"), 
+               empObject1Email = empObj1.getEmail(empObj1.setEmail("jb@gmail.com"));
+        
+        int empObjectNum1 = empObj1.getEmpNum(123456);
+        
+        
+        
+        String empObject2Name = empObj2.getName("Ana Banana"), 
+               empObject2Email = empObj2.getEmail(empObj2.setEmail("ab@gmail.com"));
+        
+        int empObjectNum2 = empObj2.getEmpNum(345213);
+        
+        
+        String empObject3Name = empObj3.getName("Tom Thumb"),
+               empObject3Email = empObj3.getEmail(empObj3.setEmail("tt@gmail.com"));
+        
+        int empObjectNum3 = empObj3.getEmpNum(542134);
