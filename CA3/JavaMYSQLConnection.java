47f2269ba33449c6d6e91275848aca839772470f
mgelezov2 <115002474+mgelezov2@users.noreply.github.com>
2024-03-31 19:26:14 +0100

    ADDS A CHECK WHICH USER LOGGED IN

--- CA3/JavaMYSQLConnection.java
+++ CA3/JavaMYSQLConnection.java
@@ -12,5 +12,31 @@ public class JavaMYSQLConnection {
         String col_username = col_userInput.nextLine();
         System.out.println("Please enter your password: ");
         String col_password = col_userInput.nextLine();
+        String loginNameQuery = "SELECT colUser_full_name, colUser_login_name, colUser_password FROM users WHERE colUser_login_name = '" + col_username + "'";
+        PreparedStatement PreparedLoginNameStatement = collegeLogin_connection.prepareStatement(loginNameQuery);
+        ResultSet loginNameSet = PreparedLoginNameStatement.executeQuery();
+        if (loginNameSet.next()) {
+            String user_FN = loginNameSet.getString("colUser_full_name");
+            String user_LN = loginNameSet.getString("colUser_login_name");
+            String user_Pass = loginNameSet.getString("colUser_password");
+            if (user_LN.equals("") || user_LN.equals(NULL)) {
+                System.out.println("User is not registered");
+            }
+            if (col_username.equals("admin") && col_password.equals(user_Pass)) {
+                System.out.println("Hello, " + user_FN);
+                user = "admin";
+            }
+            else if (col_username.equals("office") && col_password.equals(user_Pass)) {
+                System.out.println("Hello, " + user_FN);
+                user = "office";
+            }
+            else if (col_username.equals("lecturer") && col_password.equals(user_Pass)) {
+                System.out.println("Hello, " + user_FN);
+                user = "lecturer";
+            }
+            else {
+                System.out.println("Login failed. Check if your username or password was correct.");
+            }
+            }
     }
 }
