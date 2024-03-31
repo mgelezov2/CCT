5dd331d66fb7dec6e353d468282c9f8495fcaec9
mgelezov2 <115002474+mgelezov2@users.noreply.github.com>
2024-03-31 19:05:55 +0100

    ADDS STATIC VALUES FOR USE

--- OOA_CA3/JavaMYSQLConnection.java
+++ OOA_CA3/JavaMYSQLConnection.java
@@ -1,3 +1,8 @@
 public class JavaMYSQLConnection {
-    
+    private static String MySQL_url = "jdbc:mysql://localhost:3306/College_Database";
+    private static String MySQL_user = "MichaelGelezov";
+    private static String MySQL_pass = "3ur18650y2qc236";
+    private static String user;
+    private static List<String> userManagement = new ArrayList<>();
+    private static Scanner col_userInput = new Scanner(System.in);
 }
