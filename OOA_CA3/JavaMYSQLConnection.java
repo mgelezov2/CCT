0bed5891bcfa0e27839891d10fcaecd82a6eea4f
mgelezov2 <115002474+mgelezov2@users.noreply.github.com>
2024-03-31 19:12:02 +0100

    Delete OOA_CA3 directory

--- OOA_CA3/JavaMYSQLConnection.java
+++
@@ -1,8 +0,0 @@
-public class JavaMYSQLConnection {
-    private static String MySQL_url = "";
-    private static String MySQL_user = "";
-    private static String MySQL_pass = "";
-    private static String user;
-    private static List<String> userManagement = new ArrayList<>();
-    private static Scanner col_userInput = new Scanner(System.in);
-}
