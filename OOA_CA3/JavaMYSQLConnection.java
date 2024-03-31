ab202daa231598a1d115456502860a80a643340b
mgelezov2 <115002474+mgelezov2@users.noreply.github.com>
2024-03-31 19:06:50 +0100

    ADDS EMPTY MYSQL VALUES
    FILL IN URL, USER AND PASSWORD TO TEST DATABASE IN JAVA

--- OOA_CA3/JavaMYSQLConnection.java
+++ OOA_CA3/JavaMYSQLConnection.java
@@ -1,7 +1,7 @@
 public class JavaMYSQLConnection {
-    private static String MySQL_url = "jdbc:mysql://localhost:3306/College_Database";
-    private static String MySQL_user = "MichaelGelezov";
-    private static String MySQL_pass = "3ur18650y2qc236";
+    private static String MySQL_url = "";
+    private static String MySQL_user = "";
+    private static String MySQL_pass = "";
     private static String user;
     private static List<String> userManagement = new ArrayList<>();
     private static Scanner col_userInput = new Scanner(System.in);
